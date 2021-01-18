ed65ecd15a2c827d541c1bd683afd45fb409c2e2
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-01-17 21:52:16 -0500

    improved next button functionality

--- assets/main.js
+++ assets/main.js
@@ -18,9 +18,8 @@ $().ready(function() {
     }];
 
     var questionCount = 0; // counts which question user is on
-    var userSelections = []; // array containing user choices
     var score = 0;
-    var quizOver = false;
+    var highscore = 0;
     var timerButton = $(".seconds-left")
     var quizButton = $(".start-quiz");
     var introduction = $(".introduction");
@@ -38,7 +37,6 @@ $().ready(function() {
     $(".start-quiz").on("click", function() {
         introduction.hide(); //Hide the introduction container and contents
         createQandAElement(); // Create the question and answers
-        showNextQuestion(); // Display the question
         countdown(); // Start the countdown timer
     })
 
@@ -61,8 +59,6 @@ $().ready(function() {
     }
 
     function createQandAElement() {
-        var index = 0;
-
         var divEl = $("<div>", {id: 'question'});
         divEl.addClass("container-md mx-4 my-4 question-container")
         $("#quiz").append(divEl);
@@ -70,20 +66,20 @@ $().ready(function() {
         var cardEl = $("<div>").addClass("card py-4");
         divEl.append(cardEl);
 
-        var h5El = $("<h5>Question " + (index + 1) + ":</h5>");
+        var h5El = $("<h5>Question " + (questionCount + 1) + ":</h5>");
         h5El.addClass("card-title");
         cardEl.append(h5El);
 
         var questionEl = $("<p>");
         questionEl.addClass("card-text");
-        questionEl.text(questionsObj[index].question);
+        questionEl.text(questionsObj[questionCount].question);
         cardEl.append(questionEl);
 
         var answerButtons = "";
         for (var i = 0; i < questionsObj.length; i++) {
             answerButtons = $("<button class='btn btn-light mx-4 my-2 py-3 answer' name='answer'></button>");
-            answerButtons.attr("id", questionsObj[index].correctAnswer);
-            answerButtons.text(questionsObj[index].choices[i]);
+            answerButtons.attr("id", questionsObj[questionCount].correctAnswer);
+            answerButtons.text(questionsObj[questionCount].choices[i]);
             cardEl.append(answerButtons);
         }
 
@@ -92,8 +88,7 @@ $().ready(function() {
         var nextQuestionButton = $("<button>Next</button>");
         nextQuestionButton.addClass("next btn btn-light mx-4 my-2");
         cardEl.append(nextQuestionButton);
-        index++;
-        questionCount++
+        $(".next").on("click", nextQuestion);
         return divEl;
     };
 
@@ -102,20 +97,19 @@ $().ready(function() {
         
     // });
 
-
-    $(".next").on("click", function(e) {
+    function nextQuestion(e) {
         e.preventDefault();
-        if (userSelections[questionCount] == isNaN) {
-            alert("Please make a selection");
+        $("#question").hide()
+        if (questionCount === 4) {
+            showHighScores();
         } else {
-            questionCount++
-            showNextQuestion();
+            questionCount++;
+            createQandAElement();
         }
-    })
+    }
 
     function userChoice(e) {
         e.preventDefault();
-        var index = 0;
         if ($(this).id == questionsObj[0].correctAnswer) {
             alert("Correct")
         } else {
@@ -124,27 +118,8 @@ $().ready(function() {
         index++;
     }
 
-    function showNextQuestion() {
-        $("#question").remove();
-
-        if (questionCount < questionsObj.length) {
-            var nextQuestion = createQandAElement(questionCount);
-            quiz.append(nextQuestion);
-
-        }
+    function showHighScores() {
+        highscore = (score / 4);
+        seeHighscores();
     }
-    // Click handler for the 'next' button
-    // $('.next').on('click', function (e) {
-    //     e.preventDefault();
-    //     choose();
-      
-    //     // If no user selection, progress is stopped
-    //     if (userSelections[questionCount] == isNaN) {
-    //         alert('Please make a selection!');
-    //     } else {
-    //         questionCounter++;
-    //         displayNext();
-    //     }
-    // });  
-
 });
